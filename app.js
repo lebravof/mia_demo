ecb4172bef15c466f830d31ce3397f88ac0ec4e3
Luis Bravo <lbravof@gmail.com>
2024-05-21 14:20:28 -0400

    Cambio en App

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //import app from './src/mia_server.js';
 import express from 'express';
+import app from './src/mia_server.js';
 
 import { PORT } from './config.js';
-const app = express();
+//const app = express();
 
 app.listen(PORT, () => {
     console.log(`Running Express Server on Port ${PORT}`);
