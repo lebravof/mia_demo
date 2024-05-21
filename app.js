59eddc63e8bc2dc570e4be2520404b77a9882b35
Luis Bravo <lbravof@gmail.com>
2024-05-21 14:01:23 -0400

    Incorporacion de app en app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-import app from './src/mia_server.js';
+//import app from './src/mia_server.js';
+import express from 'express';
+
 import { PORT } from './config.js';
+const app = express();
 
 app.listen(PORT, () => {
     console.log(`Running Express Server on Port ${PORT}`);
--- src/mia_server.js
+++ src/mia_server.js
@@ -1,4 +1,4 @@
-import express, { request, response } from 'express';
+import express from 'express';
 import morgan from 'morgan';
 import routerCategory from './category/routerCategory.js';
 import routerFavorite from './favorite/routerFavorite.js';
