82604ed550a18791f56c018dc40b1d9416e83be5
Luis Bravo <lbravof@gmail.com>
2024-05-21 18:04:02 -0400

    Prueba conexion Railway 6

--- mia.js
+++ mia.js
@@ -1,5 +1,8 @@
-import app from './src/app.js';
+//import app from './src/app.js';
+import express from 'express';
 import { PORT } from './config.js';
+
+const app = express();
 
 app.listen(PORT, () => {
     console.log(`Running Express Server on Port ${PORT}`);
