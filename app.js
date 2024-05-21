1f6ada8be61d2dc1e7c6048d774bfa8fd91774e5
Luis Bravo <lbravof@gmail.com>
2024-05-21 12:17:10 -0400

    Change App

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import app from './src/mia_server.js'
-import { PORT } from './config.js'
+import app from './src/mia_server.js';
+import { PORT } from './config.js';
 
 app.listen(PORT, () => {
     console.log(`Running Express Server on Port ${PORT}`);
