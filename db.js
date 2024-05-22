b4bd646e9c1083f6f2055a4e8f48cadf6a7549da
Luis Bravo <lbravof@gmail.com>
2024-05-22 09:32:44 -0400

    Prueba conexion Railway - MySQL 2

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 import { createPool } from 'mysql2/promise';
 import {DB_HOST, DB_USER, DB_PASS, DB_DB, DB_PORT} from './config.js';
 
+console.log(`DB_HOST: ${DB_HOST}`);
+console.log(`DB_USER: ${DB_USER}`);
+console.log(`DB_PASS: ${DB_PASS}`);
+console.log(`DB_DB: ${DB_DB}`);
+console.log(`DB_PORT: ${DB_PORT}`);
+
 export const pool = createPool({
     host: DB_HOST,
     user: DB_USER,
